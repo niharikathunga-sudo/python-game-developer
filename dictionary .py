afc30383664c80a9152b716f22684f9f68c7f1a9
niharikathunga-sudo <niharikathunga@gmail.com>
2025-11-01 11:52:32 -0500

    Add files via upload

---
+++ dictionary .py
@@ -0,0 +1,8 @@
+people={"Niharika":14,"Saanvika":14, "Sri Akka":16, "Manya":12, "Teju":8}
+print(len(people))
+for i,j in people.items():
+    print(i,j)
+
+print(people.keys())
+print(people.values())
+print(people.items())
