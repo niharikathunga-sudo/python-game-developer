56c7fd77ba44052e95f510e519110b1d85979053
niharikathunga-sudo <niharikathunga@gmail.com>
2025-11-16 10:08:40 -0600

    Add files via upload

--- greenblob.py
+++ greenblob.py
@@ -4,8 +4,18 @@ WIDTH=500
 HEIGHT=500
 alien=Actor("greenblobthatsanalien")
 alien.pos=(50,40)
+message=""
 def draw():
     screen.fill("pink")
     alien.draw()
+    screen.draw.text(message,(10,30),fontsize=35)
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        alien.x=random.randint(50,450)
+        alien.y=random.randint(50,450)
+        message="GNARLY!!"
+    else:
+        message="OH NO YOU MISSED!"
 
 pgzrun.go()
