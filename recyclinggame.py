96b6875b29f308e5e407b5edaa06f1f253c32570
niharikathunga-sudo <niharikathunga@gmail.com>
2025-12-30 12:23:32 -0600

    Add files via upload

---
+++ recyclinggame.py
@@ -0,0 +1,92 @@
+import pgzrun, random
+HEIGHT=800
+WIDTH=600
+levels=4
+startspeed=10
+nonri=["plasticbag","batteryimg","bottleimg","chipsimg"]
+gameover=False
+gamewin=False
+currentlevel=1
+additems=[]
+animations=[]
+
+def draw():
+    screen.blit("recyclebg.png",(0,0))
+
+    if gameover:
+        screen.draw.text("OH NO, YOU LOST!! :(",fontsize=40,center=(200,100),color="white")
+    elif gamewin:
+        screen.draw.text("YAY YOU WON!! :)",fontsize=40,center=(200,100),color="white")
+
+    else:
+        for i in additems:
+            i.draw()
+def update():
+    global additems
+    if len(additems)==0:
+        additems=masteritemcreator(currentlevel)
+def masteritemcreator(numitems):
+    itemstocreate=itempicker(numitems)
+    itemtoimage=imageloader(itemstocreate)
+    itemlayout(itemtoimage)
+    animateditems(itemtoimage)
+    return itemtoimage
+
+def itempicker(numitems):
+    itemstocreate=["paperimg"]
+    for i in range(numitems):
+        randomoption=random.choice(nonri)
+        itemstocreate.append(randomoption)
+    return itemstocreate
+
+def imageloader(itemstocreate):
+    itemtoimage=[]
+    for i in itemstocreate:
+        image=Actor(i)
+        itemtoimage.append(image)
+    
+    return itemtoimage
+
+def itemlayout(itemstolayout):
+    numgaps=len(itemstolayout)+1
+    gapsize=WIDTH/numgaps
+    random.shuffle(itemstolayout)
+    for index,item in enumerate(itemstolayout,1):
+        xpos=index*gapsize
+        item.x=xpos
+
+def animateditems(itemstoanimate):
+    for i in itemstoanimate:
+        duration=startspeed-currentlevel
+        i.anchor=("center","bottom")
+        animation=animate(i,duration=duration,on_finished=handlegameover())
+        animations.append(animation)
+
+def handlegameover():
+    global gameover
+    gameover=True
+
+def on_mouse_down(pos):
+    for i in additems:
+        if i.collidepoint(pos):
+            if "paperimg" in i.image:
+                handlegamewin()
+            else:
+                handlegameover()
+
+def handlegamewin():
+    global gamewin,additems,animations
+    stopanimations(animations)
+    if currentlevel==levels:
+        gamewin=True
+    else:
+        curentlevel+=1
+        additems=[]
+        animations=[]
+        
+def stopanimations(animationstostop):
+    for i in animationstostop:
+        if i.running:
+            i.stop()
+
+pgzrun.go()
