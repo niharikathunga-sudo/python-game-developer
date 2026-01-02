a5434f250f539f4bbf2db01a1d2a6db1f499e94a
niharikathunga-sudo <niharikathunga@gmail.com>
2026-01-02 12:24:16 -0600

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,41 @@
+import pgzrun
+HEIGHT=600
+WIDTH=800
+TITLE="WELCOME TO QUIZ MASTER..."
+scrollerbox=Rect(0,0,800,80)
+questionbox=Rect(0,0,600,150)
+answerbox1=Rect(0,0,300,150)
+answerbox2=Rect(0,0,300,150)
+answerbox3=Rect(0,0,300,150)
+answerbox4=Rect(0,0,300,150)
+timerbox=Rect(0,0,200,150)
+skipbox=Rect(0,0,200,320)
+score=0
+totaltime=25
+gameover=False
+currentq=0
+totalq=0
+scrollerbox.move_ip(0,0)
+questionbox.move_ip(20,100)
+answerbox1.move_ip(20,270)
+answerbox2.move_ip(20,440)
+answerbox3.move_ip(340,270)
+answerbox4.move_ip(340,440)
+timerbox.move_ip(650,100)
+skipbox.move_ip(650,270)
+
+def draw():
+    screen.fill(color="black")
+    screen.draw.filled_rect(scrollerbox,"white")
+    screen.draw.filled_rect(questionbox,"beige")
+    screen.draw.filled_rect(answerbox1,"light pink")
+    screen.draw.filled_rect(answerbox2,"light pink")
+    screen.draw.filled_rect(answerbox3,"light pink")
+    screen.draw.filled_rect(answerbox4,"light pink")
+    screen.draw.filled_rect(timerbox,"white")
+    screen.draw.filled_rect(skipbox,"white")
+    scrollerboxmessage=f"WELCOME TO QUIZ MASTER... Q:{currentq}/{totalq}"
+    screen.draw.textbox(scrollerboxmessage,scrollerbox,color="black")
+
+
+pgzrun.go()
