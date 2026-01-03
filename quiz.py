3df4bd90a3c42c2485fe716136aed0e7de3059f6
niharikathunga-sudo <niharikathunga@gmail.com>
2026-01-03 12:21:42 -0600

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@ import pgzrun
 HEIGHT=600
 WIDTH=800
 TITLE="WELCOME TO QUIZ MASTER..."
+questions=[]
 scrollerbox=Rect(0,0,800,80)
 questionbox=Rect(0,0,600,150)
 answerbox1=Rect(0,0,300,150)
@@ -21,12 +22,12 @@ answerbox1.move_ip(20,270)
 answerbox2.move_ip(20,440)
 answerbox3.move_ip(340,270)
 answerbox4.move_ip(340,440)
-timerbox.move_ip(650,100)
+timerbox.move_ip(620,100)
 skipbox.move_ip(650,270)
 
 def draw():
     screen.fill(color="black")
-    screen.draw.filled_rect(scrollerbox,"white")
+    screen.draw.filled_rect(scrollerbox,"black")
     screen.draw.filled_rect(questionbox,"beige")
     screen.draw.filled_rect(answerbox1,"light pink")
     screen.draw.filled_rect(answerbox2,"light pink")
@@ -35,7 +36,32 @@ def draw():
     screen.draw.filled_rect(timerbox,"white")
     screen.draw.filled_rect(skipbox,"white")
     scrollerboxmessage=f"WELCOME TO QUIZ MASTER... Q:{currentq}/{totalq}"
-    screen.draw.textbox(scrollerboxmessage,scrollerbox,color="black")
+    screen.draw.textbox(scrollerboxmessage,scrollerbox,color="white")
+    screen.draw.textbox("SKIP", skipbox ,color="black",angle=-90)
+    screen.draw.textbox(str(totaltime),timerbox,color="black")
+    screen.draw.textbox(q[0],questionbox,color="black")
+    screen.draw.textbox(q[1],answerbox1,color="black")
+    screen.draw.textbox(q[2],answerbox2,color="black")
+    screen.draw.textbox(q[3],answerbox3,color="black")
+    screen.draw.textbox(q[4],answerbox4,color="black")
+    
+def update():
+    scrollerbox.x-=2
+    if scrollerbox.right<0:
+        scrollerbox.left=800
 
+def readfile():
+    global totalq
+    with open("questions.txt","r") as file:
+        for i in file:
+            questions.append(i)
+            totalq+=1
 
+def readnextq():
+    global currentq
+    currentq+=1
+    return questions.pop(0).split("|")
+
+readfile()
+q=readnextq()
 pgzrun.go()
