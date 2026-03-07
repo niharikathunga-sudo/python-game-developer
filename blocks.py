f6393aaa2609f34fe0ad57b7f4f6c471736b1300
niharikathunga-sudo <niharikathunga@gmail.com>
2026-03-07 10:17:38 -0600

    Add files via upload

--- blocks.py
+++ blocks.py
@@ -8,16 +8,26 @@ class sacrifices(pygame.sprite.Sprite):
         self.image=pygame.Surface((20,15))
         self.image.fill(color)
         self.rect=self.image.get_rect()
+        self.reset()
+
+
 
     def reset(self):
         self.rect.x=random.randint(0,800)
-        self.rect.y=random.randint(-300,-20)
+        self.rect.y=random.randint(-500,-20)
+
 
 
     def update(self):
         self.rect.y+=1
         if self.rect.y>600:
             self.reset()
+
+
+
+score=0
+
+
 
     
 class mainsacrifice(sacrifices):
@@ -35,4 +45,31 @@ red=mainsacrifice("red")
 allspriteslist.add(red)
 
 for i in range(50):
-    black=sacrifice("red")
+    black=sacrifices("black")
+    blackblocklist.add(black)
+    allspriteslist.add(black)
+
+clock=pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit()
+
+    screen.fill("white")
+
+    allspriteslist.update()
+   
+    # check if the player block has collided with ANYTHING
+
+    blockhitlist=pygame.sprite.spritecollide(red,blackblocklist,False)
+    for i in blockhitlist:
+        score+=5
+        print(f"SCORE={score}")
+        i.reset()
+
+    allspriteslist.draw(screen)
+    clock.tick(60)
+
+    pygame.display.update()
+    
